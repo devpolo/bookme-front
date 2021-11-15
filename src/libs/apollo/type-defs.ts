1c7d39a3fe61db73eb651dbda983c95f12e58516
DevPolo <paul.crussaire@gmail.com>
2021-11-15 11:30:06 +0100

    feat(✅): type defs

--- src/libs/apollo/type-defs.ts
+++ src/libs/apollo/type-defs.ts
@@ -2,18 +2,85 @@ import { gql } from "@apollo/client"
 
 export const typeDefs = gql`
   type User {
-    id: ID!
+    id: Int!
     name: String!
+    bookings: [Booking!]
   }
-  input LoginInput {
+
+  type Booking {
+    id: Int!
+    title: String!
+    start: DateTime!
+    end: DateTime!
+    roomId: Int!
+    room: Room!
+    userId: Int!
+    user: User!
+  }
+
+  """
+  A date-time string at UTC, such as 2019-12-03T09:54:33Z, compliant with the date-time format.
+  """
+  scalar DateTime
+
+  type Room {
+    id: Int!
     name: String!
+    bookings: [Booking!]
   }
 
   type Query {
-    # rooms(): User!
-   
+    rooms: [Room!]!
+    roomById(id: Int!): Room!
+    bookings: [Booking!]!
+    bookingById(id: Int!): [Booking!]!
+    user(id: Int!): User!
   }
+
   type Mutation {
-    login(input: LoginInput!): User!
+    createRoom(createRoomInput: CreateRoomInput!): Room!
+    deleteRoom(id: Int!): Boolean!
+    createBooking(createBookingInput: CreateBookingInput!): Booking!
+    updateBooking(updateBookingInput: UpdateBookingInput!): Booking!
+    removeBooking(DeleteBookingInput: DeleteBookingInput!): Boolean!
+    createUser(createUserInput: CreateUserInput!): User!
+    updateUser(updateUserInput: UpdateUserInput!): User!
+    removeUser(id: Int!): User!
+    login(name: String!): User!
+  }
+
+  input CreateRoomInput {
+    name: String!
+  }
+
+  input CreateBookingInput {
+    title: String!
+    start: DateTime!
+    end: DateTime!
+    roomId: Float!
+    userId: Float!
+  }
+
+  input UpdateBookingInput {
+    title: String
+    start: DateTime
+    end: DateTime
+    roomId: Float
+    userId: Float!
+    id: Float!
+  }
+
+  input DeleteBookingInput {
+    id: Float!
+    userId: Float!
+  }
+
+  input CreateUserInput {
+    name: String!
+  }
+
+  input UpdateUserInput {
+    name: String
+    id: Int!
   }
 `
