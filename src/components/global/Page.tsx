386e8f42b31ec02f1f8d7b5c140761033762d96b
DevPolo <paul.crussaire@gmail.com>
2021-11-15 15:48:38 +0100

    feat(✅): page component

---
+++ src/components/global/Page.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next"
+
+import Head from "next/head"
+
+const Page: NextPage = () => {
+  return (
+    <Head>
+      <title>Bookme</title>
+      <meta name='description' content='Book me app' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  )
+}
+
+export default Page
